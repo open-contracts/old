135eb201e53466d18ed7f508802657ca89e000f1
open-contracts <90533597+open-contracts@users.noreply.github.com>
2021-10-09 17:16:40 -0700

    Update main.js

--- main.js
+++ main.js
@@ -106,14 +106,13 @@ async function callFunction(fname) {
    
 }
 
-function b64ToArrayBuffer(base64) {
-    var binary_string = window.atob(base64);
-    var len = binary_string.length;
-    var bytes = new Uint8Array(len);
-    for (var i = 0; i < len; i++) {
-        bytes[i] = binary_string.charCodeAt(i);
-    }
-    return bytes.buffer;
+function hexStringToArrayBuffer(hexString) {
+    var pairs = hexString.match(/[\dA-F]{2}/gi);
+    var integers = pairs.map(function(s) {
+        return parseInt(s, 16);
+    });
+    var array = new Uint8Array(integers);
+    return array.buffer;
 }
 
 function submitOracle() {
@@ -129,7 +128,7 @@ function submitOracle() {
     ws.onmessage = function (event) {
         data = JSON.parse(event.data);
 	if (data['fname'] == "attestation") {
-	   cose = b64ToArrayBuffer(data['attestation']);
+	   cose = hexStringToArrayBuffer(data['attestation']);
 	   cbor1 = CBOR.decode(cose);
            console.log(cbor1);
 	   trusted_connection = true;
