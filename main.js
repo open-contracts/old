74a3b4a8ae377a9f49da334dc5efbf3a1366aaac
open-contracts <90533597+open-contracts@users.noreply.github.com>
2021-10-10 17:44:47 -0700

    Update main.js

--- main.js
+++ main.js
@@ -119,6 +119,22 @@ function b64Url2Buff(b64urlstring) {
   }));
 }
 
+
+const rootcert = `-----BEGIN CERTIFICATE-----
+MIICETCCAZagAwIBAgIRAPkxdWgbkK/hHUbMtOTn+FYwCgYIKoZIzj0EAwMwSTEL
+MAkGA1UEBhMCVVMxDzANBgNVBAoMBkFtYXpvbjEMMAoGA1UECwwDQVdTMRswGQYD
+VQQDDBJhd3Mubml0cm8tZW5jbGF2ZXMwHhcNMTkxMDI4MTMyODA1WhcNNDkxMDI4
+MTQyODA1WjBJMQswCQYDVQQGEwJVUzEPMA0GA1UECgwGQW1hem9uMQwwCgYDVQQL
+DANBV1MxGzAZBgNVBAMMEmF3cy5uaXRyby1lbmNsYXZlczB2MBAGByqGSM49AgEG
+BSuBBAAiA2IABPwCVOumCMHzaHDimtqQvkY4MpJzbolL//Zy2YlES1BR5TSksfbb
+48C8WBoyt7F2Bw7eEtaaP+ohG2bnUs990d0JX28TcPQXCEPZ3BABIeTPYwEoCWZE
+h8l5YoQwTcU/9KNCMEAwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUkCW1DdkF
+R+eWw5b6cp3PmanfS5YwDgYDVR0PAQH/BAQDAgGGMAoGCCqGSM49BAMDA2kAMGYC
+MQCjfy+Rocm9Xue4YnwWmNJVA44fA0P5W2OpYow9OYCVRaEevL8uO1XYru5xtMPW
+rfMCMQCi85sWBbJwKKXdS6BptQFuZbT73o/gBh1qUxl/nNr12UO8Yfwr6wPLb+6N
+IwLz3/Y=
+-----END CERTIFICATE-----`;
+
 async function ifValidExtractRSAkey(attestation_data) {
     // validates attestation, and extracts enclave's RSA pubkey if succesfull
     const cose = hexStringToArrayBuffer(attestation_data);
@@ -128,23 +144,22 @@ async function ifValidExtractRSAkey(attestation_data) {
     console.log("ENCLAVE HASHES:-----------", attestation_doc['pcrs'])
     const certificate = new x509.X509Certificate(new Uint8Array(attestation_doc['certificate']));
     var valid = false;
+
     // checks attestation signature
     await certificate.publicKey.export()
     .then(key=>window.crypto.subtle.exportKey("jwk", key))
     .then(function (key) {b64Url2Buff(key['y']); return key})
     .then(key=>COSE.verify(b64Url2Buff(key['x']), b64Url2Buff(key['y']), cose));
+
     // checks certificate path
     var certs = [];
     const cabundle = attestation_doc['cabundle'];
-    console.log(cabundle);
-    for (var i=0; i<cabundle.length; i++) {
-    	var cert = new Uint8Array(cabundle[i]);
-    	console.log('raw', cert);
-	    var cert = new x509.X509Certificate(cert);
-	    console.log(cert);
-	    certs.push(cert);
+    for (var i=0; i<cabundle.length-1; i++) {
+        var cert = new Uint8Array(cabundle[i]);
+        var cert = new x509.X509Certificate(cert);
+        certs.push(cert);
     }
-    console.log(certs);
+    certs.push(new x509.X509Certificate(rootcert))
     const chain = new x509.X509ChainBuilder({certificates: certs});
     const items = await chain.build(certificate);
     return true;
