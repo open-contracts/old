4545fabced18fa0569bf78012021365f2f5e7d47
open-contracts <90533597+open-contracts@users.noreply.github.com>
2021-10-29 02:28:02 -0700

    Update main.js

--- main.js
+++ main.js
@@ -92,7 +92,7 @@ function showFunction(fname) {
       currentFunction += `<div>	<label for="msgValue">messageValue (ETH):	</label> <input id="msgValue" type="text" value="0" size="60" /></div>`;
   }
   if (requires_oracle) {
-      currentFunction += `<div>	<label for="getOracleFolder">Load Oracle Data:    </label> <input id="getOracleFolder" type="submit" onclick="getOracleFolder('${fjson.oracle_folder}')"/></div>`;
+      currentFunction +=`<br /> <input id="getOracleFolder" type="submit" value="Load Oracle Data" onclick=getOracleFolder("${fjson.oracle_folder}")})/> </form>`;
       currentFunction +=`<br /> <input id="callButton" type="submit" value="Call" onclick=getOracleIP(}) disabled="true"/> </form>`;
   } else {
       for (let i = 0; i < fjson.inputs.length; i++) {
