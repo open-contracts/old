d37a166aa99437b0bd2db584687497b656f8d2be
Jonas Metzger <40152568+Jonas-Metzger@users.noreply.github.com>
2021-11-02 23:18:17 -0700

    Update main.js

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ async function loadOpenContract() {
 
 
 async function getTokens() {
-    await OPNtoken.gimmeSomeMoreOfDemCoins();
+    await OPNtoken.gimmeTen();
 }
 
 async function allowHub() {
