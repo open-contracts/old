87d14c8cca082bacb8d9349eade568453f3da352
open-contracts <90533597+open-contracts@users.noreply.github.com>
2021-11-04 19:48:23 -0700

    Update main.js

--- main.js
+++ main.js
@@ -105,7 +105,7 @@ async function  loadOpenContract() {
     var interface = await OpenContracts(window);
     const link = "https://raw.githubusercontent.com/" + $('#contractGithub').val();
     const contract_interface = JSON.parse(await (await fetch(new URL(link + "/interface.json"))).text());
-    const oc_interface = JSON.parse(await (await fetch('opencontracts_interface.json')).text();
+    const oc_interface = JSON.parse(await (await fetch('opencontracts_interface.json')).text());
     interface.parseContracts(oc_interface, contract_interface);
     fnButtons = "<p><b>Contract Functions:</b></p>";
     for (let i = 0; i < interface.contractFunctions.length; i++) {
