9effaba013b0ab868c68926fcd1e9dcb1f41ec9d
open-contracts <90533597+open-contracts@users.noreply.github.com>
2021-11-04 21:30:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -30,16 +30,19 @@ async function showFunction(f) {
         currentFunction += `<div><label for="${f.inputs[i].name}"> ${f.inputs[i].name} (${f.inputs[i].description}):</label> <input id="${f.inputs[i].name}" type="text" value="" size="60" /></div>`;
     }
     if (f.requiresOracle) {
+        document.getElementById('callButton').disabled = true;
         f.printHandler = printHandler;
         f.inputHander = inputHandler;
         f.xpraHandler = xpraHandler;
         f.errorHandler = errorHandler;
         const [user, repo, ref] =  $('#contractGithub').val().split("/");
-        window["oracleLoader"] = async function () {f.oracleData = await githubOracleDownloader(user, repo, ref, f.oracleFolder)};
-        currentFunction += `<div><label for="loadOracle">Load Oracle Data (this may take a bit): </label><input id="loadOracle" type="submit" value="Load" onclick="window.oracleLoader()" /></div>`;       
+        window["oracleLoader"] = async function () {
+            document.getElementById('loadOracle').disabled = true;
+            f.oracleData = await githubOracleDownloader(user, repo, ref, f.oracleFolder)
+            document.getElementById('callButton').disabled = false;
+        };
+        currentFunction += `<div><label for="loadOracle">Load Oracle Data (this may take a bit): </label><input id="loadOracle" type="submit" value="Load" onclick="window.oracleLoader()" /></div>`;  
     }
-
-    currentFunction +=`<br> <br> <input id="callButton" type="submit" value="Call" onclick="${'window.call'+f.name}()" /> </form>`;
     $('#currentFunction').html(currentFunction);
     window['call' + f.name] = async function () {
         for (let i = 0; i < f.inputs.length; i++) {f.inputs[i].value = $(`#${f.inputs[i].name}`).val()}
