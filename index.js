61416716d14f7ed33c585f6a1e9dd3545980daaa
open-contracts <90533597+open-contracts@users.noreply.github.com>
2021-11-04 20:27:18 -0700

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ async function showFunction(f) {
         for (let i = 0; i < f.inputs.length; i++) {f.inputs[i].value = $(`#${f.inputs[i].name}`).val()}
         $('#results').html(await f.call());
     };
-    currentFunction +=`<br /> <input id="callButton" type="submit" value="Call" onclick="${'window.call'+f.name}()" /> </form>`;
+    currentFunction +=`<br> <br> <input id="callButton" type="submit" value="Call" onclick="${'window.call'+f.name}()" /> </form>`;
     $('#currentFunction').html(currentFunction);
     $('#results').html("");
 }
