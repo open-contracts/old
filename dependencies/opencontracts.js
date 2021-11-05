57ebd4f2cd87d8cf7f5f82ae09deb8dff1919dd3
open-contracts <90533597+open-contracts@users.noreply.github.com>
2021-11-05 12:25:35 -0700

    Update opencontracts.js

--- dependencies/opencontracts.js
+++ dependencies/opencontracts.js
@@ -4,6 +4,73 @@ function hexStringToArray(hexString) {
     return new Uint8Array(integers);
 }
 
+function b64Url2Buff(b64urlstring) {
+  return new Uint8Array(atob(b64urlstring.replace(/-/g, '+').replace(/_/g, '/')).split('').map(val => {
+    return val.charCodeAt(0);
+  }));
+}
+
+
+const rootcert = `-----BEGIN CERTIFICATE-----
+MIICETCCAZagAwIBAgIRAPkxdWgbkK/hHUbMtOTn+FYwCgYIKoZIzj0EAwMwSTEL
+MAkGA1UEBhMCVVMxDzANBgNVBAoMBkFtYXpvbjEMMAoGA1UECwwDQVdTMRswGQYD
+VQQDDBJhd3Mubml0cm8tZW5jbGF2ZXMwHhcNMTkxMDI4MTMyODA1WhcNNDkxMDI4
+MTQyODA1WjBJMQswCQYDVQQGEwJVUzEPMA0GA1UECgwGQW1hem9uMQwwCgYDVQQL
+DANBV1MxGzAZBgNVBAMMEmF3cy5uaXRyby1lbmNsYXZlczB2MBAGByqGSM49AgEG
+BSuBBAAiA2IABPwCVOumCMHzaHDimtqQvkY4MpJzbolL//Zy2YlES1BR5TSksfbb
+48C8WBoyt7F2Bw7eEtaaP+ohG2bnUs990d0JX28TcPQXCEPZ3BABIeTPYwEoCWZE
+h8l5YoQwTcU/9KNCMEAwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUkCW1DdkF
+R+eWw5b6cp3PmanfS5YwDgYDVR0PAQH/BAQDAgGGMAoGCCqGSM49BAMDA2kAMGYC
+MQCjfy+Rocm9Xue4YnwWmNJVA44fA0P5W2OpYow9OYCVRaEevL8uO1XYru5xtMPW
+rfMCMQCi85sWBbJwKKXdS6BptQFuZbT73o/gBh1qUxl/nNr12UO8Yfwr6wPLb+6N
+IwLz3/Y=
+-----END CERTIFICATE-----`;
+
+
+// extracts pubkeys and enclave hash if attestation doc is valid
+async function extractContentIfValid(attestation_data) {
+    // decode COSE_SIGN1 message
+    const cose = hexStringToArray(attestation_data).buffer;
+    const cose_sign1_struct = CBOR.decode(cose);
+    const array = new Uint8Array(cose_sign1_struct[2]);
+    const attestation_doc = CBOR.decode(array.buffer);
+
+    // check attestation signature
+    const certificate = new x509.X509Certificate(new Uint8Array(attestation_doc['certificate']));
+    await certificate.publicKey.export()
+    .then(key=>window.crypto.subtle.exportKey("jwk", key))
+    .then(function (key) {b64Url2Buff(key['y']); return key})
+    .then(key=>COSE.verify(b64Url2Buff(key['x']), b64Url2Buff(key['y']), cose));
+
+    // check certificate path
+    root = new x509.X509Certificate(rootcert);
+    var certs = [root];
+    const cabundle = attestation_doc['cabundle'];
+    for (var i=1; i<cabundle.length; i++) {
+        var cert = new Uint8Array(cabundle[i]);
+        var cert = new x509.X509Certificate(cert);
+        certs.push(cert);
+    }
+    const chain = new x509.X509ChainBuilder({certificates: certs});
+    const items = await chain.build(certificate);
+    const validcertpath = await root.equal(items[items.length-1]);
+    if (!validcertpath) {throw new Error('Invalid Certpath in Attestation')}
+
+    // extracts hash + pubkeys
+    const hash = attestation_doc['pcrs'][0];
+    console.log("------->UNCHECKED< ENCLAVE HASH:--------", hash);
+    // TODO: Add hash ceck
+    const ETHkey = new TextDecoder().decode(attestation_doc['public_key']);
+    const RSAraw = hexStringToArray(new TextDecoder().decode(attestation_doc['user_data'])).buffer;
+    const RSAkey = await crypto.subtle.importKey('spki', RSAraw, {name: "RSA-OAEP", hash: "SHA-256"}, true, ["encrypt"]);
+    const AESkey = await crypto.subtle.generateKey({"name":"AES-GCM","length":256},true,['encrypt','decrypt']);
+    const rawAES = new Uint8Array(await crypto.subtle.exportKey('raw', AESkey));
+    const encryptedAESkey = await Base64.fromUint8Array(new Uint8Array(await window.crypto.subtle.encrypt({name: "RSA-OAEP"}, RSAkey, rawAES)));
+    return [ETHkey, AESkey, encryptedAESkey];
+}
+
+
+
 async function enclaveSession(interface, f) {
     const registryIP = hexStringToArray(await interface.OPNhub.registryIpList(0)).join(".");
     var ws = new WebSocket("wss://" + registryIP + ":8080/");
