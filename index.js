95a7ed3d7a7587decfcd18a58481a6119bfd8612
open-contracts <90533597+open-contracts@users.noreply.github.com>
2021-11-04 20:08:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ async function loadOpenContract() {
     fnButtons = "<p><b>Contract Functions:</b></p>";
     for (let i = 0; i < interface.contractFunctions.length; i++) {
         fname = interface.contractFunctions[i].name;
-        fnButtons += `<input id=${fname} type="submit" value="${fname}" onclick="showFunction(${interface.contractFunctions[i]})" />`;
+        const wrapper = function () showFunction(interface.contractFunctions[i]);
+        fnButtons += `<input id=${fname} type="submit" value="${fname}" onclick="wrapper()" />`;
     }
     $('#functionNames').html(fnButtons);
     $('#currentFunction').html("");
