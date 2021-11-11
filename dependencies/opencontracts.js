e450d0b3a609faa01b968aae89c2295650a6b1e9
open-contracts <90533597+open-contracts@users.noreply.github.com>
2021-11-10 22:13:21 -0800

    Update opencontracts.js

--- dependencies/opencontracts.js
+++ dependencies/opencontracts.js
@@ -181,6 +181,7 @@ async function enclaveSession(opencontracts, f) {
 		    userInput = await f.inputHandler(data['message']);
 		    ws.send(JSON.stringify(await encrypt(AESkey, {fname: 'user_input', input: userInput})));
 		} else if (data['fname'] == 'submit') {
+		    console.log('calling submit handler');
 		    await f.submitHandler(async function() {
 		        return await requestHubTransaction(opencontracts, data['nonce'], data['calldata'], data['oracleSignature'],
 							    data['oracleProvider'], data['registrySignature']);
